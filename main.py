c5085be2b4e85dfc6621bd2125a08b9a623e749d
kvoloshenko <kvoloshenko@gmail.com>
2022-04-28 16:26:49 +0300

    Add project

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 import requests
 import json
 from flask import Flask
-# text = requests.get('https://www.python.org/').text
-# https://www.cbr-xml-daily.ru/
-# https://www.cbr-xml-daily.ru/daily_json.js
 
-#text = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').text
 
-#currencies = json.loads(text)
-#print(currencies)
-
-#print(currencies['Valute'])
-
-#for currency in currencies ['Valute']:
-#    print(currency)
+def get_valutes_list():
+    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    valutes = list(data['Valute'].values())
+    return valutes
 
 
 app = Flask(__name__)
 
+
+def create_html(valutes):
+    text = '<h1>Курс валют</h1>'
+    text += '<table>'
+    text += '<tr>'
+    for _ in valutes[0]:
+        text += f'<th><th>'
+    text += '</tr>'
+    for valute in valutes:
+        text += '<tr>'
+        for v in valute.values():
+            text += f'<td>{v}</td>'
+        text += '</tr>'
+
+    text += '</table>'
+    return text
+
+
 @app.route("/")
-def hello():
-    text = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').text
-    currencies = json.loads(text)
-    result = ''
-    for currency in currencies['Valute']:
-        result += str(currency) + '<br>'
-    return result
+def index():
+    valutes = get_valutes_list()
+    html = create_html(valutes)
+    return html
+
 
 if __name__ == "__main__":
     app.run()
